4062f6bfe685ce0c444439c0fd051501de71256c
Cat Norris <cannenorris@yahoo.com>
2017-11-15 01:41:05 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 speakers=["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
 
-def batch_badge_creator (name)
+def badge_maker (name)
   new_array=[]
   name.each do |speaker|
     new_array << ("Hello, my name is #{speaker}.")
