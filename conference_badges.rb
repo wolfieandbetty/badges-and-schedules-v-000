59fab70e6a1a9436cdfb44f8dec1bc5d6b26d376
Cat Norris <cannenorris@yahoo.com>
2017-11-15 01:31:52 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,5 @@
 # Write your code here.
-speakers=["Edsger","Ada","Charles","Alan","Grace,"Linus","Matz"]
+speakers=["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
 
 def batch_badge_creator (speakers)
   speakers.each do |name|
